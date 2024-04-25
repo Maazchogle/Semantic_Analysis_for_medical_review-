682578aabb7a945aed20a607334a714f2bf56621
Maazchogle <110540925+Maazchogle@users.noreply.github.com>
2024-04-25 18:01:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request
-from tensorflow.keras.models import load_model
 import pickle
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
 
 app = Flask(__name__)
 
-# Load the Keras model and TF-IDF vectorizer
-model = load_model('model.keras')
+# Load the SVM model and TF-IDF vectorizer
+with open('svm_model_subset.pkl', 'rb') as model_file:
+    svm_model_subset = pickle.load(model_file)
 
-with open('tfidf_vectorizer.pkl', 'rb') as vectorizer_file:
-    tfidf_vectorizer = pickle.load(vectorizer_file)
+with open('tfidf_vectorizer_subset.pkl', 'rb') as vectorizer_file:
+    tfidf_vectorizer_subset = pickle.load(vectorizer_file)
 
 # Define a route for the home page
 @app.route('/')
@@ -23,13 +24,13 @@ def predict():
         review = request.form['review']
         
         # Vectorize the input review using the TF-IDF vectorizer
-        review_tfidf = tfidf_vectorizer.transform([review])
+        review_tfidf = tfidf_vectorizer_subset.transform([review])
         
-        # Make a prediction using the Keras model
-        prediction = model.predict(review_tfidf)
+        # Make a prediction using the SVM model
+        prediction = svm_model_subset.predict(review_tfidf)[0]
         
         # Map the prediction to a human-readable label and emoji
-        if prediction > 0.5:
+        if prediction == 'positive':
             sentiment_label = 'Positive 😃'
             emoji = '😃'
         else:
@@ -40,3 +41,50 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+from flask import Flask, render_template, request
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+
+app = Flask(__name__)
+
+# Load the SVM model and TF-IDF vectorizer
+with open('svm_model_subset.pkl', 'rb') as model_file:
+    svm_model_subset = pickle.load(model_file)
+
+with open('tfidf_vectorizer_subset.pkl', 'rb') as vectorizer_file:
+    tfidf_vectorizer_subset = pickle.load(vectorizer_file)
+
+# Define a route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# Define a route for sentiment prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        review = request.form['review']
+        
+        # Vectorize the input review using the TF-IDF vectorizer
+        review_tfidf = tfidf_vectorizer_subset.transform([review])
+        
+        # Make a prediction using the SVM model
+        prediction = svm_model_subset.predict(review_tfidf)[0]
+        
+        # Map the prediction to a human-readable label and emoji
+        if prediction == 'positive':
+            sentiment_label = 'Positive 😃'
+            emoji = '😃'
+        else:
+            sentiment_label = 'Negative 😞'
+            emoji = '😞'
+
+        return render_template('result.html', review=review, sentiment=sentiment_label, emoji=emoji)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
